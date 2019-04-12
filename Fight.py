7d22a4fd072baf0f45ec10e023fdbf0f247f20d7
Zahari Tudzharov <zhistler@gmail.com>
2019-04-12 14:18:59 +0300

    Spell and fight work correct

--- Fight.py
+++ Fight.py
@@ -9,28 +9,30 @@ class Fight:
         if type(hero) != Hero or type(enemy) != Enemy:
             raise ValueError("Those types can't fight!")
         while hero.is_alive() and enemy.is_alive():
-            if hero.magic != None:
+            if hero.spell != None:
                 if hero.can_cast():
                     hero.attack('spell')
-                    enemy.take_damage(hero.magic.spell_damage)
+                    enemy.take_damage(hero.spell.spell_damage)
                 else:
-                    hero.magic = None
+                    hero.spell = None
             elif hero.weapon != None:
                 hero.attack('weapon')
                 enemy.take_damage(hero.weapon.damage)
             else:
                 print('Hero is not equipt.')
             if enemy.is_alive():
-                if enemy.magic != None:
-                    if enemy.can_cast():
-                        enemy.attack('spell')
-                        hero.take_damage(enemy.magic.spell_damage)
-                    else:
-                        enemy.magic = None
-                elif enemy.weapon != None:
-                    enemy.attack('weapon')
-                    hero.take_damage(enemy.weapon.damage)
+                if hero.spell.cast_range > 0 and hero.spell != None:
+                    hero.spell.cast_range -= 1
                 else:
-                    enemy.damage
-                    hero.take_damage(enemy.damage)
-Fight().fight_simulator(1,2)
+                    if enemy.spell != None:
+                        if enemy.can_cast():
+                            enemy.attack('spell')
+                            hero.take_damage(enemy.spell.spell_damage)
+                        else:
+                            enemy.spell = None
+                    elif enemy.weapon != None:
+                        enemy.attack('weapon')
+                        hero.take_damage(enemy.weapon.damage)
+                    else:
+                        enemy.damage
+                        hero.take_damage(enemy.damage)
--- Spell.py
+++ Spell.py
@@ -1,7 +1,8 @@
 class Spell:
-    def __init__(self, name, damage, mana_cost):
+    def __init__(self, name, damage, mana_cost, cast_range):
         if type(name) != str or type(damage) != int or type(mana_cost) != int:
             raise ValueError('Not correct type!') 
         self.spell_name = name
         self.spell_damage = damage
         self.mana_cost = mana_cost
+        self.cast_range = cast_range
